757f38bf21a1bf0ee5404aeb5f7fba80b69fc45d
Dario Moceri <dario.moceri@bitfinex.com>
2025-05-21 19:54:39 +0200

    fix signer

--- src/wallet-spark-signer.js
+++ src/wallet-spark-signer.js
@@ -13,11 +13,15 @@
 // limitations under the License.
 'use strict'
 
-import { DefaultSparkSigner } from '@buildonspark/spark-sdk/signer'
-import { hexToBytes, bytesToHex } from '@noble/curves/abstract/utils'
-import { getMasterHDKeyFromSeed, Network, ValidationError } from '@buildonspark/spark-sdk'
 
-export default class WalletSparkSigner extends DefaultSparkSigner {
+import { hexToBytes, bytesToHex } from '@noble/curves/abstract/utils'
+import { getMasterHDKeyFromSeed, Network, ValidationError } from '@buildonspark/spark-sdk/utils'
+
+
+import { ReactNativeSparkSigner, DefaultSparkSigner } from '@buildonspark/spark-sdk/signer'
+const SparkSigner = ReactNativeSparkSigner || DefaultSparkSigner
+
+export default class WalletSparkSigner extends SparkSigner {
   constructor (index = 0) {
     super()
 
