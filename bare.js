f574393996677da7d6eee2af058d0b30616bc46d
Davide Casale <davi0kprogramsthings@gmail.com>
2025-08-14 18:02:34 +0200

    Refactor code.

--- bare.js
+++ bare.js
@@ -16,4 +16,5 @@
 import 'bare-wdk-runtime'
 
 export * from './index.js' with { imports: 'bare-wdk-runtime/package' }
+
 export { default } from './index.js' with { imports: 'bare-wdk-runtime/package' }
